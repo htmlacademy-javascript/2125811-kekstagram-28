978d4b629c14dc865e2d40fe508d4173cce6f9e5
DenisNilov <denis-nilov@yandex.ru>
2023-03-09 15:28:05 +0300

    В процессе выполнения Таск 7

--- js/util.js
+++ js/util.js
@@ -1,1 +1,29 @@
-//в процессе выполнения задания 5
+const getRandomInteger = (a, b) => {
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+//Функция возвращает случайный элемент массива
+const getRandomArrayElements = (elements) => elements [getRandomInteger(0, elements.length - 1)];
+
+// Функция для генерации уникального (неповторяющегося) числа из указанного дипазона
+function createRandomIdFromRangeGenerator (min, max) {
+  const previousValues = [];
+  return function () {
+    let currentValue = getRandomInteger(min, max);
+    while (previousValues.includes(currentValue)) {
+      currentValue = getRandomInteger(min, max);
+    }
+    previousValues.push(currentValue);
+    return currentValue;
+  };
+}
+
+
+export {
+  getRandomInteger,
+  getRandomArrayElements,
+  createRandomIdFromRangeGenerator
+};
