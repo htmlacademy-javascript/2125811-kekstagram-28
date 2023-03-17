772c52e7786cdc00fa562a6b5b7dfb5c61243870
DenisNilov <denis-nilov@yandex.ru>
2023-03-17 20:58:01 +0300

    Исправил 8-1

--- js/rendering-bigPhoto.js
+++ js/rendering-bigPhoto.js
@@ -12,8 +12,12 @@ const bigPictureDescription = document.querySelector('.social__caption');
 const socialCommentElement = bigPictureCommentsList.querySelector('.social__comment');
 const socialCommentElementCounter = pictureModalElement.querySelector('.social__comment-count');
 const socialCommentElementLoader = pictureModalElement.querySelector('.comments-loader');
+//let commentsShow = 0;
+//let commentis = [];
+//const COMMENT_PER_POSSITION = 5;
 
-const renderingBigPicture = function () {
+const renderingBigPicture = () => {
+  //функция при нажатии Escape
   const onBigPictureKeydown = (evt) => {
     if(isEscapeKey(evt)) {
       evt.preventDefault();
@@ -24,14 +28,8 @@ const renderingBigPicture = function () {
   };
 
 
-  const bigPictureRending = function (evt) {
-    const currentThumbnail = evt.target.closest('.picture__img');
-    const thumbnailPointer = currentThumbnail.id;
-    const {url, description, likes, comments} = photoObjectsArray[thumbnailPointer];
-    bigPictureLikes.textContent = likes;
-    bigPictureCommentsNum.textContent = comments.length;
-    bigPictureImage.src = url;
-    bigPictureDescription.textContent = description;
+  //Генерируем коменты
+  const createComments = (comments) => {
     const commentsFragment = document.createDocumentFragment();
     comments.forEach((comment) => {
       const socialCommentCopy = socialCommentElement.cloneNode(true);
@@ -41,31 +39,51 @@ const renderingBigPicture = function () {
       bigPictureCommentsList.innerHTML = '';
       commentsFragment.append(socialCommentCopy);
     });
-
     bigPictureCommentsList.append(commentsFragment);
   };
 
-  const openBigPicture = function () {
+  //Загружаем комменты
+
+
+  //Подставляю в большую фотку из массива
+  const bigPictureRending = (evt) => {
+    const currentThumbnail = evt.target.closest('.picture__img');
+    const thumbnailPointer = currentThumbnail.id;
+    const {url, description, likes, comments} = photoObjectsArray[thumbnailPointer];
+    bigPictureLikes.textContent = likes;
+    bigPictureCommentsNum.textContent = comments.length;
+    bigPictureImage.src = url;
+    bigPictureDescription.textContent = description;
+    createComments(comments);
+
+  };
+
+
+  //функция открывания большого фото
+  const openBigPicture = () => {
     pictureModalElement.classList.remove('hidden');
     bodyElement.classList.add('modal-open');
     document.addEventListener('keydown', onBigPictureKeydown);
-    socialCommentElementCounter.classList.add('hidden');
-    socialCommentElementLoader.classList.add('hidden');
+    //socialCommentElementCounter.classList.add('hidden');
+    //socialCommentElementLoader.classList.add('hidden');
   };
 
-  const closeBigPicture = function () {
+  //функция закрывания большого фото
+  const closeBigPicture = () => {
     pictureModalElement.classList.add('hidden');
     bodyElement.classList.remove('modal-open');
     document.removeEventListener('keydown', onBigPictureKeydown);
     bigPictureCommentsList.innerHTML = '';
   };
 
+  //обработчик события клик по миниатюре
   picturesList.addEventListener('click', (evt) => {
     evt.preventDefault();
     openBigPicture();
     bigPictureRending(evt);
   });
 
+  //обработчик события клик по кнопке
   pictureCancelButton.addEventListener('click', () => {
     closeBigPicture();
   });
@@ -73,3 +91,22 @@ const renderingBigPicture = function () {
 };
 
 export {renderingBigPicture};
+
+/* const renderComment = () => {
+
+  commentsShow += COMMENT_PER_POSSITION;
+  if (commentsShow >= commentis) {
+    socialCommentElementLoader.classList.add('hidden');
+    commentsShow = commentis.length;
+  } else {
+    socialCommentElementLoader.classList.remove('hidden');
+  }
+  const fragment = document.createDocumentFragment();
+  for (let i = 0; i < commentsShow; i++) {
+    const commentElement = createComments(comments[i]);
+    fragment.append(commentElement);
+  }
+  bigPictureCommentsList.innerHTML = '';
+  bigPictureCommentsList.append(fragment);
+  socialCommentElementCounter.innerHTML = `${commentsShow} из <span class= "comments-count">${commentis.length}<span> комментариев`;
+}; */
