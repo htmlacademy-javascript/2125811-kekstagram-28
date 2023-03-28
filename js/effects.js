4e75ebc2bad0bf0ade665a753b8f65bf27ebcdee
DenisNilov <denis-nilov@yandex.ru>
2023-03-29 00:12:40 +0300

    выполнил 9-2

---
+++ js/effects.js
@@ -0,0 +1,132 @@
+const EFFECTS = [
+  {
+    name: 'none',
+    style: 'none',
+    min: 0,
+    max: 100,
+    step: 1,
+    unit: '',
+  },
+  {
+    name: 'chrome',
+    style: 'grayscale',
+    min: 0,
+    max: 1,
+    step: 0.1,
+    unit: '',
+  },
+  {
+    name: 'sepia',
+    style: 'sepia',
+    min: 0,
+    max: 1,
+    step: 0.1,
+    unit: '',
+  },
+  {
+    name: 'marvin',
+    style: 'invert',
+    min: 0,
+    max: 100,
+    step: 1,
+    unit: '%',
+  },
+  {
+    name: 'phobos',
+    style: 'blur',
+    min: 0,
+    max: 3,
+    step: 0.1,
+    unit: 'px',
+  },
+  {
+    name: 'heat',
+    style: 'brightness',
+    min: 1,
+    max: 3,
+    step: 0.1,
+    unit: '',
+  },
+];
+
+const DEFAULT_EFFECT = EFFECTS[0];
+
+const pictureElement = document.querySelector('.img-upload__preview img');
+const effectsElement = document.querySelector('.effects');
+const sliderElement = document.querySelector('.effect-level__slider');
+const sliderContainerElement = document.querySelector('.img-upload__effect-level');
+const effectLevelElement = document.querySelector('.effect-level__value');
+
+
+const showSlider = () => {
+  sliderContainerElement.classList.remove('hidden');
+};
+
+
+const hideSlider = () => {
+  sliderContainerElement.classList.add('hidden');
+};
+
+let chosenEffect = DEFAULT_EFFECT;
+
+const isDefaultValue = () => chosenEffect === DEFAULT_EFFECT;
+
+
+const updateSlider = () => {
+  sliderElement.noUiSlider.updateOptions({
+    range: {
+      min: chosenEffect.min,
+      max: chosenEffect.max,
+    },
+    step: chosenEffect.step,
+    start: chosenEffect.max,
+  });
+  if (isDefaultValue()) {
+    hideSlider();
+  } else {
+    showSlider();
+  }
+};
+
+
+const onEffectClick = (evt) => {
+  if (!evt.target.classList.contains('effects__radio')) {
+    return;
+  }
+
+  chosenEffect = EFFECTS.find((effect) => effect.name === evt.target.value);
+  pictureElement.className = `effects__preview--${chosenEffect.name}`;
+  updateSlider();
+};
+
+//
+const onSliderUpdate = () => {
+  const sliderValue = sliderElement.noUiSlider.get();
+  pictureElement.style.filter = isDefaultValue()
+    ? DEFAULT_EFFECT.style
+    : `${chosenEffect.style}(${sliderValue}${chosenEffect.unit})`;
+  effectLevelElement.value = sliderValue;
+};
+
+
+const resetEffects = () => {
+  chosenEffect = DEFAULT_EFFECT;
+  updateSlider();
+};
+
+
+noUiSlider.create(sliderElement, {
+  range: {
+    min: DEFAULT_EFFECT.min,
+    max: DEFAULT_EFFECT.max,
+  },
+  start: DEFAULT_EFFECT.max,
+  step: DEFAULT_EFFECT.step,
+  connect: 'lower',
+});
+hideSlider();
+
+effectsElement.addEventListener('change', onEffectClick);
+sliderElement.noUiSlider.on('update', onSliderUpdate);
+
+export {resetEffects};
